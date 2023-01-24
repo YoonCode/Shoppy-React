28de0acf6a3d0965be2c298e230c2d75d6e3580c
coder.yoon <yjin830201@gmail.com>
2023-01-24 11:25:49 +0900

    edit css in pricecard

--- src/components/PriceCard.jsx
+++ src/components/PriceCard.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 
 export default function PriceCard({ text, price }) {
   return (
-    <div className="bg-gray-50 p-8 mx-2 rounded-2xl text-center text-lg md:text-xl">
+    <div className="bg-gray-50 p-4 sm:p-8 mx-2 rounded-2xl text-center text-sm sm:text-lg md:text-xl">
       <p>{text}</p>
-      <p className="font-bold text-brand text-xl md:text-2xl">₩{price}</p>
+      <p className="font-bold text-brand text-lg sm:text-xl md:text-2xl">
+        ₩{price}
+      </p>
     </div>
   )
 }
